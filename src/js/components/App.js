001bfa9756859325f47c31b75df49b348d4fd261
uini223 <bartnik.emil@gmail.com>
2020-04-26 11:59:20 +0200

    improved routes and tabs

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { Form } from './Form/Form.component';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { TopBar } from './topbar/topbar';
+import { Home } from './home/home.component';
 
+const getRoutes = () => {
+    return [...menuItems].reverse().map((menuItem) => {
+        return (
+            <Route key={menuItem.label} path={menuItem.path}>
+                {menuItem.component}
+            </Route>
+        );
+    });
+};
 export const App = () => {
     return (
         <BrowserRouter>
-            <TopBar />
-            <Switch>
-                <Route path="/history">
-                    <h2>history</h2>
-                </Route>
-                <Route path="/tasks">
-                    <h2>tasks</h2>
-                </Route>
-                <Route path="/">
-                    <Form />
-                </Route>
-            </Switch>
+            <TopBar menuItems={menuItems} />
+            <Switch>{getRoutes()}</Switch>
         </BrowserRouter>
     );
 };
+
+const menuItems = [
+    { label: 'Home', path: '/', icon: 'pi pi-fw pi-home', component: <Form /> },
+    {
+        label: 'Tasks',
+        path: '/tasks',
+        icon: 'pi pi-fw pi-calendar',
+        component: <Home />,
+    },
+    // { label: 'History', path: '/history', icon: 'pi pi-fw pi-pencil' },
+];
--- src/js/components/topbar/topbar.js
+++ src/js/components/topbar/topbar.js
@@ -2,20 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { TabMenu } from 'primereact/tabmenu';
 import { useHistory, useLocation } from 'react-router-dom';
 
-const getItemByPath = (path) => {
-    return menuItems.find((menuItem) => menuItem.path === path);
-};
-
 export const TopBar = (props) => {
+    const getItemByPath = (path) => {
+        return props.menuItems?.find((menuItem) => menuItem.path === path);
+    };
     const history = useHistory();
     const [activeItem, setActiveItem] = useState(
         getItemByPath(history.location.pathname)
     );
-    const location = useLocation();
 
     return (
         <TabMenu
-            model={menuItems}
+            model={props.menuItems}
             activeItem={activeItem}
             onTabChange={({ value }) => {
                 history.push(value.path);
@@ -24,9 +22,3 @@ export const TopBar = (props) => {
         />
     );
 };
-
-const menuItems = [
-    { label: 'Home', path: '/', icon: 'pi pi-fw pi-home' },
-    { label: 'Tasks', path: '/tasks', icon: 'pi pi-fw pi-calendar' },
-    { label: 'History', path: '/history', icon: 'pi pi-fw pi-pencil' },
-];
